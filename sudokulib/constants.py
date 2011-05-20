fe3e552ab054105fb461b1556320fa7fcf189409
Fantomas42 <fantomas42@gmail.com>
2011-05-20 11:09:58 +0200

    adding constants and layer modules

---
+++ sudokulib/constants.py
@@ -0,0 +1,5 @@
+"""Constants for sudokulib"""
+
+GRID_WIDTH = 9
+
+GRID_TOTAL = GRID_WIDTH * GRID_WIDTH
---
+++ sudokulib/layer.py
@@ -0,0 +1,21 @@
+"""Layer for sudokulib"""
+from sudokulib.constants import GRID_WIDTH
+
+
+class Layer(object):
+    """Class layer for abstracting and manipulating Grid"""
+
+    def __init__(self, data_str, solution_str, mystery_char='X'):
+        self.str = ''
+        for i in range(len(data_str)):
+            if not solution_str[i] in (mystery_char, ' '):
+                self.str += solution_str[i]
+            else:
+                self.str += data_str[i]
+        
+        self.row_table = []
+        for i in range(GRID_WIDTH):
+            self.row_table.append(list(self.str[i:i + GRID_WIDTH]))
+
+    def __str__(self):
+        return self.str
