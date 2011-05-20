3a8d6c001c89a0d737b9852cac4f56cdfb9df13e
Fantomas42 <fantomas42@gmail.com>
2011-05-20 11:19:08 +0200

    preparing tests

---
+++ sudokulib/tests/__init__.py
@@ -0,0 +1,4 @@
+"""Tests for sudokulib"""
+from sudokulib.tests.layer import LayerTestCase
+
+
---
+++ sudokulib/tests/layer.py
@@ -0,0 +1,10 @@
+"""Tests for Layer"""
+from unittest import TestCase
+
+from sudokulib.layer import Layer
+
+
+class LayerTestCase(TestCase):
+
+    def test_str(self):
+        self.assertEquals(1, 1)
