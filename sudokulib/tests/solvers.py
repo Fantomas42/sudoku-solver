237ed4ebd6c68439bc634e03f5f5d79bbc668368
Fantomas42 <fantomas42@gmail.com>
2011-05-20 21:52:00 +0200

    adding test for singleton solver

--- sudokulib/tests/solvers.py
+++ sudokulib/tests/solvers.py
@@ -4,17 +4,32 @@ from unittest import TestCase
 from sudokulib.layer import Layer
 from sudokulib.solvers import SingletonSolver
 
-DATA_SET = '1234567X957813962449687215395238146764129' \
-           '7835387564291719623548864915372235748916'
+
 SOLUTION = '                                         ' \
            '                                        '
 
 
 class SingletonSolverTestCase(TestCase):
-
-    def setUp(self):
-        self.layer = Layer(DATA_SET, SOLUTION)
+    """Tests for SingletonSolver"""
 
     def test_solve(self):
-        solver = SingletonSolver(self.layer, 7)
+        # Row resolution
+        data = '1234567X957813962449687215395238146764129' \
+               '7835387564291719623548864915372235748916'
+        layer = Layer(data, SOLUTION)
+        solver = SingletonSolver(layer, 7)
         self.assertEquals(solver.solve(), '8')
+
+        # Col resolution
+        data = '123X56X8957813962449687215395238146764129' \
+               '7835387564291719623548864915372235748916'
+        layer = Layer(data, SOLUTION)
+        solver = SingletonSolver(layer, 3)
+        self.assertEquals(solver.solve(), '4')
+
+        # Blokc resolution
+        data = '123X56X89578139624496872153952381467641X9' \
+               '7835387564291719623548864915372235748916'
+        layer = Layer(data, SOLUTION)
+        solver = SingletonSolver(layer, 3)
+        self.assertEquals(solver.solve(), '4')
