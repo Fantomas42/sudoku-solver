be969e3dbab08a245cc530df3605757c0c434e2d
Fantomas42 <fantomas42@gmail.com>
2011-05-26 22:09:05 +0200

    adding a complete test for LineBlockPreprocessor

--- sudokulib/preprocessors.py
+++ sudokulib/preprocessors.py
@@ -1,4 +1,6 @@
 """Preprocessors for sudokulib"""
+from sudokulib.constants import GRID_WIDTH
+from sudokulib.constants import BLOCK_WIDTH
 
 
 class BasePreprocessor(object):
@@ -14,4 +16,15 @@ class LineBlockPreprocessor(BasePreprocessor):
     name = 'Block Col/Row Interaction'
 
     def preprocess(self, layer):
+        # Detecter un jumeau ou triple dans les regions
+        #   => Un jumeau est 2 candidats qui se suivent sur une ligne
+        #      ou une colonne nul par ailleurs dans une region
+        # Si jumeau ou triple :
+        #   Supprimer les candidats de la ligne/colonne hors de la region
+
+
+        for i in range(GRID_WIDTH):
+            pass
+
+
         return None
--- sudokulib/tests/preprocessors.py
+++ sudokulib/tests/preprocessors.py
@@ -10,11 +10,41 @@ SOLUTION = ' ' * 81
 class LineBlockPreprocessorTestCase(TestCase):
     """Tests for LineBlockPreprocessor"""
 
-    def test_solve(self):
+    def test_preprocess(self):
         preprocessor = LineBlockPreprocessor()
 
         data = '29X7X5X481XX6892738X7X249X5XXX2X1XXXXX24X' \
                '68XXXX98X342X9XX5X2XX44XXXX8X92321947586'
 
         layer = Layer(data, SOLUTION)
-        # TODO write a test
+        self.assertEquals(layer._candidates[27], set(['5', '6', '7']))
+        self.assertEquals(layer._candidates[28], set(['3', '4', '5', '6', '7', '8']))
+        self.assertEquals(layer._candidates[29], set(['3', '4', '5', '6', '8']))
+        self.assertEquals(layer._candidates[33], set(['3', '6', '7']))
+        self.assertEquals(layer._candidates[34], set(['3', '5', '6']))
+        self.assertEquals(layer._candidates[35], set(['7', '9']))
+
+        layer = LineBlockPreprocessor().preprocess(layer)
+        self.assertEquals(layer._candidates[27], set(['5', '7']))
+        self.assertEquals(layer._candidates[28], set(['3', '4', '5', '7', '8']))
+        self.assertEquals(layer._candidates[29], set(['3', '4', '5', '8']))
+        self.assertEquals(layer._candidates[33], set(['3', '6', '7']))
+        self.assertEquals(layer._candidates[34], set(['3', '5', '6']))
+        self.assertEquals(layer._candidates[35], set(['7', '9']))
+
+        data = '29X7X5X481XX6892738X7X249X5XXX2X1XXXXX24X' \
+               '68XX6X98X342X9XX5X2XX44XXXX8X92321947586'
+
+        layer = Layer(data, SOLUTION)
+        self.assertEquals(layer._candidates[28], set(['3', '4', '5', '7', '8']))
+        self.assertEquals(layer._candidates[37], set(['1', '3', '5', '7']))
+        self.assertEquals(layer._candidates[46], set(['1', '5', '7']))
+        self.assertEquals(layer._candidates[55], set(['6', '7', '8']))
+        self.assertEquals(layer._candidates[64], set(['5', '6', '7']))
+
+        layer = LineBlockPreprocessor().preprocess(layer)
+        self.assertEquals(layer._candidates[28], set(['3', '4', '5', '8']))
+        self.assertEquals(layer._candidates[37], set(['1', '3', '5']))
+        self.assertEquals(layer._candidates[46], set(['1', '5']))
+        self.assertEquals(layer._candidates[55], set(['6', '7', '8']))
+        self.assertEquals(layer._candidates[64], set(['5', '6', '7']))
