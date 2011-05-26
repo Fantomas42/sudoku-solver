90e27268192d7e998d0eb14f894d9db1e9477c74
Fantomas42 <fantomas42@gmail.com>
2011-05-26 20:34:47 +0200

    pyflakes ok and test suite written

--- sudokulib/tests/__init__.py
+++ sudokulib/tests/__init__.py
@@ -1,7 +1,18 @@
 """Tests for sudokulib"""
+import unittest
+
 from sudokulib.tests.layer import LayerTestCase
 from sudokulib.tests.layer import AdvancedLayerTestCase
-from sudokulib.tests.solvers import LineBlockSolverTestCase
 from sudokulib.tests.solvers import SingletonSolverTestCase
 from sudokulib.tests.solvers import NakedSingletonSolverTestCase
 from sudokulib.tests.solvers import HiddenSingletonSolverTestCase
+
+loader = unittest.TestLoader()
+
+test_suite = unittest.TestSuite([
+    loader.loadTestsFromTestCase(LayerTestCase),
+    loader.loadTestsFromTestCase(AdvancedLayerTestCase),
+    loader.loadTestsFromTestCase(SingletonSolverTestCase),
+    loader.loadTestsFromTestCase(NakedSingletonSolverTestCase),
+    loader.loadTestsFromTestCase(HiddenSingletonSolverTestCase),
+    ])
