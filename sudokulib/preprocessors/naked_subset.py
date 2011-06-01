ed6e56483c9ccfed7ff2e2b990e7f7159d98e7be
Fantomas42 <fantomas42@gmail.com>
2011-06-01 17:07:00 +0200

    pep8 cleanup

--- sudokulib/preprocessors/naked_subset.py
+++ sudokulib/preprocessors/naked_subset.py
@@ -17,19 +17,18 @@ class NakedSubsetPreprocessor(BasePreprocessor):
                 for index in indexes:
                     candidates = layer._candidates[index]
                     candidates = ''.join(sorted(list(candidates)))
-                    candidates_indexes.setdefault(candidates, [])
-                    candidates_indexes[candidates].append(index)
+                    candidates_indexes.setdefault(candidates, set())
+                    candidates_indexes[candidates].add(index)
                 for key, item in candidates_indexes.items():
                     key_len = len(key)
                     item_len = len(item)
                     if key_len > 1 and key_len == item_len:
-                        for candidate_to_remove in list(key):
-                            for index in indexes:
-                                if index not in item and \
-                                       candidate_to_remove in layer._candidates[index]:
-                                    layer_has_change = True
-                                    layer._candidates[index] = layer._candidates[
-                                        index] - set(candidate_to_remove)
+                        key_set = set(key)
+                        for index in set(indexes) - item:
+                            if key_set & layer._candidates[index]:
+                                layer_has_change = True
+                                layer._candidates[index] = layer._candidates[
+                                    index] - key_set
                 if layer_has_change:
                     return layer
         return None
