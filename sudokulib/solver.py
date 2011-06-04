4acaf989985eb39e8177872139b44240adbadefb
Fantomas42 <fantomas42@gmail.com>
2011-06-04 18:13:07 +0200

    better message on backtracking

--- sudokulib/solver.py
+++ sudokulib/solver.py
@@ -74,8 +74,8 @@ class SudokuSolver(object):
 
         if self.backtracking:
             if self.verbosity == 2:
-                print 'Sorry, but I am too dumb to solve ' \
-                      'this grid, I will use BackTracking method.'
+                print 'Sorry, but I am actually too dumb to solve ' \
+                      'this grid, I will use a BackTracking method.'
             return BacktrackingSolver().solve(layer)
 
         return solutions
