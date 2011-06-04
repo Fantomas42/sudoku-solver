658b8cb58d2518fc4449ed2bca14a3bc20d4fae5
Fantomas42 <fantomas42@gmail.com>
2011-06-04 17:20:50 +0200

    simplify backtracking

--- sudokulib/backtracking.py
+++ sudokulib/backtracking.py
@@ -3,6 +3,8 @@ from sudokulib.layer import Layer
 from sudokulib.solvers import BaseSolver
 from sudokulib.constants import GRID_TOTAL
 
+VOID_SOLUTION = ' ' * GRID_TOTAL
+
 
 class BacktrackingSolver(BaseSolver):
     """Backtrack solver"""
@@ -10,32 +12,35 @@ class BacktrackingSolver(BaseSolver):
 
     def solve(self, layer):
         solutions = []
-        missing = ''.join(layer.table).count(layer.mystery_char)
+        missings_str = ''.join(layer.table)
+        missings = missings_str.count(layer.mystery_char)
 
-        if not missing:
-            return True
+        if missings == 1:
+            missing_index = missings_str.index(layer.mystery_char)
+            missing_candidates = layer._candidates[missing_index]
+            return [[missing_index, missing_candidates.pop()]]
 
-        candidate_index = sorted([(len(layer._candidates[i]), i)
-                                  for i in range(GRID_TOTAL)
-                                  if len(layer._candidates[i])])
-        if missing and not candidate_index:
+        candidate_indexes = []
+        for i in range(GRID_TOTAL):
+            candidates = layer._candidates[i]
+            if candidates:
+                candidate_indexes.append((len(candidates), i))
+
+        if not candidate_indexes:
             return None
 
-        best_index = candidate_index[0][1]
+        best_index = sorted(candidate_indexes)[0][1]
 
         for candidate in layer._candidates[best_index]:
             layer_table = layer.table[:]
             layer_table[best_index] = str(candidate)
             solution_str = ''.join(layer_table)
-            new_layer = Layer(solution_str, ' ' * GRID_TOTAL)
+            new_layer = Layer(solution_str, VOID_SOLUTION)
             new_solution = self.solve(new_layer)
             if not new_solution:
                 continue
-            if new_solution:
-                if isinstance(new_solution, list):
-                    solutions = [[best_index, candidate]]
-                    solutions.extend(new_solution)
-                else:
-                    solutions = [[best_index, candidate]]
+            else:
+                solutions = [[best_index, candidate]]
+                solutions.extend(new_solution)
 
         return solutions
